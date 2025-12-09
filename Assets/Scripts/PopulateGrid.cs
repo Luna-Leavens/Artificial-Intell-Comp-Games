9756f3634a9d50a29dc76ce5afe59a4321002df8
VGrider <agrider1@charlotte.edu>
2025-12-09 18:05:40 -0500

    PopulateGrid rewrite complete

--- Assets/Scripts/PopulateGrid.cs
+++ Assets/Scripts/PopulateGrid.cs
@@ -20,17 +20,50 @@ public class PopulateGrid : MonoBehaviour {
 	void Start () {
 		int blockSize = (int) useGrid.cellSize.x;
 
+		Debug.Log(useGrid.cellSize.x);
+
 		row = width / blockSize;
 		column = length / blockSize;
 
-		populateGrid();
+		populateGrid(blockSize);
 
 		if (blocks != null) {
 			grid = new AStarGrid(length, width, useGrid, blocks);
 		}
 	}
 
-	/* Needs Rewrite */ void populateGrid () {
-		
+	void populateGrid (int blockSize) {
+		float startingX = length / 2 * -1;
+		float startingY = width / 2 * -1; 
+		int arrayIt = 0;
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				float currentX = startingX + (blockSize * j);
+				float currentY = startingY + (blockSize * i);
+
+				Vector3 currentPos = new Vector3(currentX, currentY, 0.0f);
+
+				Collider[] collide = Physics.OverlapSphere(currentPos, blockSize);
+
+				foreach (var detect in collide) {
+					if (detect.tag == "Companion Avoid") {
+						GridBlocks input = new GridBlocks(currentX, currentY, true, i, j);
+
+						blocks[arrayIt] = input;
+
+						arrayIt++;
+					}
+
+					else {
+						GridBlocks input = new GridBlocks(currentX, currentY, false, i, j);
+
+						blocks[arrayIt] = input;
+
+						arrayIt++;
+					}
+				}
+			}
+		}
 	}
 }
